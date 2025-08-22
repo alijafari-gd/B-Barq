c3a249348d8b0d3e6356e5573fa624349f1b016e
AJ <alijafari.gd@gmail.com>
2025-08-22 22:59:18 +0330

    refactor to Barghe-Man api

--- app/src/main/java/com/aliJafari/bbarq/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/aliJafari/bbarq/ui/auth/LoginActivity.kt
@@ -1,16 +1,33 @@
 package com.aliJafari.bbarq.ui.auth
+
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.aliJafari.bbarq.ui.auth.viewmodel.LoginUiState
 import com.aliJafari.bbarq.ui.auth.viewmodel.LoginViewModel
+
 
 class LoginActivity : ComponentActivity() {
     private val vm: LoginViewModel by viewModels()
@@ -19,33 +36,42 @@ class LoginActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                LoginScreen(vm,this)
+                LoginScreen(vm, this)
             }
         }
     }
 }
 
 @Composable
-fun LoginScreen(vm: LoginViewModel,context: Context) {
+fun LoginScreen(vm: LoginViewModel, context: Context) {
     val state by vm.uiState.collectAsState()
     var phone by remember { mutableStateOf("") }
     var code by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), verticalArrangement = Arrangement.Center
     ) {
         when (state) {
             is LoginUiState.EnterPhone -> {
-                OutlinedTextField(value = phone, onValueChange = { phone = it }, label = { Text("Phone") })
+                OutlinedTextField(
+                    value = phone,
+                    onValueChange = { phone = it },
+                    label = { Text("Phone") })
                 Spacer(Modifier.height(12.dp))
                 Button(onClick = { vm.sendOtp(phone) }) { Text("Send Code") }
+                Spacer(Modifier.height(12.dp))
+                Text("ATTENTION : Logging into Bargheman.com", color = MaterialTheme.colorScheme.error)
             }
 
             is LoginUiState.EnterCode -> {
-                OutlinedTextField(value = code, onValueChange = { code = it }, label = { Text("SMS Code") })
+                OutlinedTextField(
+                    value = code,
+                    onValueChange = { code = it },
+                    label = { Text("SMS Code") })
                 Spacer(Modifier.height(12.dp))
-                Button(onClick = { vm.verifyOtp(context,code) }) { Text("Verify Code") }
+                Button(onClick = { vm.verifyOtp(context, code) }) { Text("Verify Code") }
             }
 
             is LoginUiState.Loading -> {
@@ -58,6 +84,15 @@ fun LoginScreen(vm: LoginViewModel,context: Context) {
 
             is LoginUiState.Success -> {
                 Text("Login successful. Token: ${(state as LoginUiState.Success).token.take(20)}...")
+                Spacer(Modifier.height(12.dp))
+                Button(onClick = {
+                    val i: Intent = context.getPackageManager()
+                        .getLaunchIntentForPackage(context.getPackageName())!!
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(i)
+                    System.exit(0)
+                }) { Text("Restart App") }
             }
         }
     }
